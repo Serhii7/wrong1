ace0c485911f997ebe0608b82252170f08a0f330
we <sergiyp777@gmail.com>
2017-05-08 00:43:56 +0300

    added news

---
+++ models/news.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var newsSchema = new Schema({
+	new_id:String,
+	user_id_who:String,
+	user_id:String,
+	date:String,
+	text:String,
+	event:String
+});
+
+var news = mongoose.model('News', newsSchema);
+
+module.exports = news;
---
+++ public/newsClient.js
@@ -0,0 +1,22 @@
+$(function () {
+
+	function getCookie(name) {
+	  var matches = document.cookie.match(new RegExp(
+	    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	  ));
+	  return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+
+	var token = getCookie("token");
+	      // $('body').append('<div class="alert alert-info" role="alert">Hi</div>');
+	var socket = io.connect();
+	socket.emit('news',token);
+
+	socket.on('news', function(msg){
+	      $('body').append('<div class="alert alert-info" role="alert">'+msg+'</div>');
+	      $('.alert').fadeIn(1000);
+		
+	      $('.alert').fadeOut(3000);
+	    });
+
+	});
